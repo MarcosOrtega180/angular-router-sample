b23e6e004da35cc5e810c07b9cfe13c327ea899a
marcos <marcos.ortega94@gmail.com>
2019-02-05 14:11:34 -0300

    page not found

--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -7,17 +7,21 @@ import {CrisisListComponent} from './crisis-list/crisis-list.component';
 import {HeroListComponent} from './hero-list/hero-list.component';
 import {FormsModule} from "@angular/forms";
 import {RouterModule, Routes} from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
     {path:'crisis-center', component:CrisisListComponent},
-    {path:'heroes', component:HeroListComponent}
+    {path:'heroes', component:HeroListComponent},
+    {path:'', redirectTo:'/heroes', pathMatch: 'full'},
+    {path:'**', component:PageNotFoundComponent}
 ]
 
 @NgModule({
     declarations: [
         AppComponent,
         CrisisListComponent,
-        HeroListComponent
+        HeroListComponent,
+        PageNotFoundComponent
     ],
     imports: [
         BrowserModule,
