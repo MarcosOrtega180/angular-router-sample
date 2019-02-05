73815927ec71fed1cba46e6a34a6521ccf7965a2
MarcosOrtega <maortega94@gmail.com>
2019-02-05 14:41:49 -0300

    milestone1 finished

--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -26,10 +26,7 @@ const appRoutes: Routes = [
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot(
-            appRoutes,
-            {enableTracing: true} //<--debugging purposes only
-        ),
+        RouterModule.forRoot(appRoutes,{enableTracing: true}), /*<--debugging purposes only*/
         AppRoutingModule
     ],
     providers: [],
