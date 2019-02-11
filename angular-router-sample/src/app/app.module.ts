b116ee291015aae3e5db7ec5bf38f83afc3e9022
MarcosOrtega <maortega94@gmail.com>
2019-02-11 17:17:42 -0300

    Milestone 6: guards in progress

--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -11,16 +11,21 @@ import {HeroesModule} from "./heroes/heroes.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { CrisisCenterModule } from './crisis-center/crisis-center.module';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
+import {AdminModule} from "./admin/admin.module";
+import { LoginComponent } from './auth/login/login.component';
+import {AuthModule} from "./auth/auth.module";
 
 
 
 @NgModule({
     imports: [
-        BrowserAnimationsModule,
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         HeroesModule,
         CrisisCenterModule,
+        AdminModule,
+        AuthModule,
         AppRoutingModule
     ],
     declarations: [
