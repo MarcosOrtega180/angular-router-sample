123e7f5e97cfcd6d1c52875cd2a8f4bc6fc3b1b0
maros <marcos.ortega94@gmail.com>
2019-02-13 08:58:23 -0300

    control

---
+++ angular-router-sample/src/app/crisis-center/crisis.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {MessageService} from "../message.service";
+import {BehaviorSubject} from "rxjs";
+import {Crisis} from "./crisis";
+import {CRISES} from "./mock-crisises";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrisisService {
+
+
+    static nextCrisisId = 100;
+    private crises$: BehaviorSubject<Crisis[]> = new BehaviorSubject<Crisis[]>(CRISES);
+    constructor(private messageService: MessageService) { }
+
+    getCrises() { return this.crises$; }
+
+    getCrisis(id: number | string) {
+        return this.getCrises().pipe(
+            map(crises => crises.find(crisis => crisis.id === +id))
+        );
+    }
+
+    addCrisis(name: string) {
+        name = name.trim();
+        if (name) {
+            let crisis = { id: CrisisService.nextCrisisId++, name };
+            CRISES.push(crisis);
+            this.crises$.next(CRISES);
+        }
+    }
+}
